424fc7463c2022afb18ba18a11f17ab89bbee69f
A48254-JoseMenezes <jmgilmenezes17@gmail.com>
2022-10-07 16:19:41 +0100

    Started validate ship fleet fun

--- src/main/kotlin/server_domain/FleetLayout.kt
+++ src/main/kotlin/server_domain/FleetLayout.kt
@@ -11,11 +11,32 @@ data class FleetLayout(
             validate()
     }
 
-    private fun validate() {
+    private fun validate(): Boolean{
+        requireNotNull(validation)
+
         /**
          * TODO: Validate Ship positions:
          *  - No Ship part falls out of board
          *  - Every ship is complete
          **/
+
+        ships.forEach{ ship ->
+            ship.parts.forEach{shipPart->
+                if( shipPart.coordinates.col !in 0 until validation.boardDimensions.cols_num )
+                    throw Exception("error on validate ships need to specify" )
+
+                if( shipPart.coordinates.row !in 0 until validation.boardDimensions.rows_num)
+                    throw Exception("error on validate ships need to specify" )
+            }
+        }
+
+        validation.shipConfiguration.forEach{shipConfiguration ->
+            if(shipConfiguration.quantity != ships.count { ship -> ship.parts.size == shipConfiguration.ship_size })
+                throw Exception("error on validate ships need to specify" )
+        }
+
+
+
+        return true
     }
 }
